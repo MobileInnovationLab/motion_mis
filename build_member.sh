e9c4af518c03e63668305c88b768d00479b4bb2c
alifgiant <alif.akbar@bukalapak.com>
2021-08-18 20:40:25 +0800

    set dependency jobs and remove build generated data

--- build_member.sh
+++ build_member.sh
@@ -1,4 +1,2 @@
 # pip3 install -r requirements.txt
-
 python3 python_scrapper/retrieve_data.py $LINKEDIN_EMAIL $LINKEDIN_PASS
-# cp -r members_data_generated flutter/
--- build_web.sh
+++ build_web.sh
@@ -1,5 +1,14 @@
+# copy generated data
+rm -rf flutter/members_data_generated
+mv members_data_generated flutter/
+
+# move directory
 cd flutter
+
+# build
 flutter build web --release
+
+# set output
 rm -rf ../docs
 mkdir ../docs
 mv ./build/web/* ../docs
